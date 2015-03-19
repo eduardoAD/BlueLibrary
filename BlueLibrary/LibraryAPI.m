10e2409d70f102801477fcd18426bacc80c7c123
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2015-03-19 13:24:01 -0600

    Facade Pattern

--- BlueLibrary/LibraryAPI.m
+++ BlueLibrary/LibraryAPI.m
@@ -7,10 +7,30 @@
 //
 
 #import "LibraryAPI.h"
+#import "PersistencyManager.h"
+#import "HTTPClient.h"
+
+@interface LibraryAPI (){
+    PersistencyManager *persistencyManager;
+    HTTPClient *httpClient;
+    BOOL isOnline;
+}
+
+@end
 
 @implementation LibraryAPI
 
-+(LibraryAPI*)sharedInstance{
+- (id)init{
+    self = [super init];
+    if (self) {
+        persistencyManager = [[PersistencyManager alloc]init];
+        httpClient = [[HTTPClient alloc]init];
+        isOnline = NO;
+    }
+    return self;
+}
+
++ (LibraryAPI*)sharedInstance{
 
 // 1 - static variable to hold the instance of your class
     static LibraryAPI *_sharedInstance = nil;
@@ -23,6 +43,24 @@
         _sharedInstance = [[LibraryAPI alloc] init];
     });
     return _sharedInstance;
+}
+
+- (NSArray*)getAlbums{
+    return [persistencyManager getAlbums];
+}
+
+- (void)addAlbum:(Album *)album atIndex:(int)index{
+    [persistencyManager addAlbum:album atIndex:index];
+    if (isOnline) {
+        [httpClient postRequest:@"/api/addAlbum" body:[album description]];
+    }
+}
+
+- (void)deleteAlbumAtIndex:(int)index{
+    [persistencyManager deleteAlbumAtIndex:index];
+    if (isOnline) {
+        [httpClient postRequest:@"/api/deleteAlbum" body:[@(index) description]];
+    }
 }
 
 @end
