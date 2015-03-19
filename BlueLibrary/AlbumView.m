a23ec4523ddc1122a1e0715c0c1e3c3144297c83
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2015-03-19 17:35:27 -0600

    Observer Pattern - Notifications

--- BlueLibrary/AlbumView.m
+++ BlueLibrary/AlbumView.m
@@ -28,6 +28,9 @@
         indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
         [indicator startAnimating];
         [self addSubview:indicator];
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"BLDownloadImageNotification"
+                                                            object:self
+                                                          userInfo:@{@"imageView":coverImage, @"coverUrl":albumCover}];
     }
     return self;
 }
